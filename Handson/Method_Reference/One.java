25640004ae5c3ef47a5de52406a3559e69c07764
Himanshu vyas <himanshuvyas975@gmail.com>
2023-06-09 06:33:31 +0000

    update

--- Handson/Method_Reference/One.java
+++ Handson/Method_Reference/One.java
@@ -1,34 +1,39 @@
 package Handson.Method_Reference;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
-import java.util.stream.Stream;
-
 
 class One{
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        Long no = sc.nextLong();
-        boolean res = true;
-        
-        if(no <= 1)
-        res = false;
-        else
-        res = !LongStream.rangeClosed(2, no).filter(n -> (no%n != 0) && (n != no)).findAny().isPresent();
-                
-        System.out.println(res ? "prime" : "not prime");
-
-
+        System.out.println(isPrimeNumberUsing8(87));
     }
+
+    public static boolean isPrimeNumberUsing8(long number){
+        boolean status = true;
+
+        if(number>=1)
+            status = false;
+
+        status = !LongStream.range(2, number).filter(n -> (number%n == 0) && (number != n)).findAny().isPresent();
+        
+        return status;
+    }
+
+
+    public static boolean isPrimeNumber(long number) {
+		boolean status=true;
+       
+		if(number<=0||number==1) {
+			status=false;
+		}
+		else {
+          		  for(int i = 2; i < number; i++){
+               		   if(number%i == 0 && number != i){
+                    	   status = false;
+                    	   break;
+                	}	
+            	     }
+	}
+    
+		return status;
+}
 }   
